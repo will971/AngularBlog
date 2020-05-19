d83ae77516b892536d0b08617b83ddce1039dbad
William's MBP <modeste.william.s@gmail.com>
2020-05-19 15:53:16 +0200

    Authentification ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import*as firebase from 'firebase'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularBlog';
+
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyBOB4kaqhtFwmDyp2NeuIbDoujnEwyktYg",
+      authDomain: "angularblog-a7d1e.firebaseapp.com",
+      databaseURL: "https://angularblog-a7d1e.firebaseio.com",
+      projectId: "angularblog-a7d1e",
+      storageBucket: "angularblog-a7d1e.appspot.com",
+      messagingSenderId: "211786756620",
+      appId: "1:211786756620:web:230d32072f5637a8283429"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
---
+++ src/app/services/books.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
+
+
+@Injectable()
+export class BooksService {
+
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data) => {
+        this.books = data.val() ? data.val() : [];
+        this.emitBooks();
+      }
+      );
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+}
